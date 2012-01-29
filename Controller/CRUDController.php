472c794895e2baa8f892e344405d940a4f732f54
Thomas Rabaix <thomas.rabaix@ekino.com>
2012-01-29 16:48:16 +0100

    Refactor node navigation

---
+++ Controller/CRUDController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sonata\DoctrinePHPCRAdminBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class CRUDController extends Controller
+{
+
+}
+
